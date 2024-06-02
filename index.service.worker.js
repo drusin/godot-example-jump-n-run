36f59a0762e201891cacf3177fbd7c0c7fca1fb8
drusin <drusin@users.noreply.github.com>
2024-06-02 14:17:19 +0000

    deploy: b27586df44c8566e62491a0cb56cb93bf4688dd2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1717337645|5083505';
+const CACHE_VERSION = '1717337837|5014557';
 /** @type {string} */
 const CACHE_PREFIX = 'Jump-N-Run-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
