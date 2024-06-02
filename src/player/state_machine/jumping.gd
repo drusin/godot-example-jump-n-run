c750cc64952ee3dc1ef55712e8efe79ce051c25f
drusin <drusin@users.noreply.github.com>
2024-06-02 14:14:07 +0000

    deploy: ba2c9b245d3eff907652041d45f2df43f6b77fc8

--- src/player/state_machine/jumping.gd
+++
@@ -1,6 +0,0 @@
-extends PlayerState
-
-
-func process(_delta: float) -> void:
-	player.velocity.y = player.JUMP_VELOCITY
-	switch.emit(name, "Air")
--- src/player/state_machine/player_state.gd
+++
@@ -1,7 +0,0 @@
-class_name PlayerState
-extends State
-
-var player: CharacterBody2D
-var animation_player: AnimationPlayer
-var sprite: Sprite2D
-var input: PlayerInput
